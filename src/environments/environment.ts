ea91af5cb77c95ec87ba296f94cc8d2aa4f2ebf6
badurubalaji <badurubalaji@gmail.com>
2023-04-08 19:10:21 +0530

    2 files modified

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,17 +9,19 @@ export const environment = {
     api_key: '5579980505863a3f6aabd82.89189525',
     site_id: 659913,
     notify_url: 'https://YOUR_NOTIFY_URL',
-    currency: 'CFA'
+    currency: 'INR',
   },
-  firebase : {
-    apiKey: "AIzaSyAGFtnvDdtupogina4NVcGHzAMPzLpZcbc",
-    authDomain: "angularpos-379cd.firebaseapp.com",
-    databaseURL: "https://angularpos-379cd.firebaseio.com",
-    projectId: "angularpos-379cd",
-    storageBucket: "angularpos-379cd.appspot.com",
-    messagingSenderId: "112372376465",
-    appId: "1:112372376465:web:e4e9f2240c18eeabeb7030"  
-  }
+  firebase: {
+    apiKey: 'AIzaSyBlcXRvYBMnzlIJuy1tOdwBuoGikR8LTHo',
+    authDomain: 'deblabs-pos.firebaseapp.com',
+    databaseURL:
+      'https://deblabs-pos-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'deblabs-pos',
+    storageBucket: 'deblabs-pos.appspot.com',
+    messagingSenderId: '1059604697027',
+    appId: '1:1059604697027:web:a5b04a77850e6d1942b776',
+    measurementId: 'G-X1XCLZGQ56',
+  },
 };
 
 /*
